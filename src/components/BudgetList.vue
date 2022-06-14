05cdca37ad41f0e92e709e0e9883de551068cc4b
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-06-14 17:22:19 +0300

    first commit, added card title

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="budget-list-wrapp">
+    <ElCard :header="header">
+
+    </ElCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BadgetList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({ })
+    }
+  },
+  data: () => ({
+    header: 'Budget List',
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
