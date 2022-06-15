63e737e235a0068e3ebd3c069cb44e191b2d5fb2
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-06-15 16:29:29 +0300

    form dane

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Form @submitForm="onFormSubmit" />
     <TotalBalance :total="totalBalance" />
     <BudgetList :list="list" @deleteItem="onDeleteItem" />
   </div>
@@ -8,12 +9,14 @@
 <script>
 import BudgetList from '@/components/BudgetList'
 import TotalBalance from '@/components/TotalBalance'
+import Form from '@/components/Form'
 
 export default {
   name: 'App',
   components: {
     BudgetList,
     TotalBalance,
+    Form,
   },
   data: () => ({
     list: {
@@ -29,7 +32,7 @@ export default {
         comment: 'Some outcome comment',
         id: 2,
       }
-    }
+    },
   }),
   computed: {
     totalBalance() {
@@ -40,6 +43,13 @@ export default {
     onDeleteItem(id) {
       this.$delete(this.list, id);
     },
+    onFormSubmit(data) {
+      const newObj = {
+        ...data,
+        id: String(Math.random())
+      };
+  this.$set(this.list, newObj.id, newObj);
+    }
   },
 }
 </script>
